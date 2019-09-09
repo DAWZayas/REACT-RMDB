caa3d36c96ca526b5e6f94b0c8f6736a1ef6c695
Javier Pérez Arteaga <surtich@gmail.com>
2019-09-09 17:46:10 +0200

    [Header] Added some images

--- src/layout/Header.tsx
+++ src/layout/Header.tsx
@@ -1,7 +1,20 @@
 import React from "react";
+import logo from "../assets/images/reactMovie_logo.png";
+import tmdb_logo from "../assets/images/tmdb_logo.png";
 
 const Header: React.FC = () => {
-  return React.createElement("div", { className: "header" }, "Header");
+  return (
+    <header className="header">
+      <div className="header__logo-box">
+        <img className="header__logo" src={logo} alt="logo"></img>
+        <img
+          className="header__tmdb-logo"
+          src={tmdb_logo}
+          alt="tmdb-logo"
+        ></img>
+      </div>
+    </header>
+  );
 };
 
 export default Header;
